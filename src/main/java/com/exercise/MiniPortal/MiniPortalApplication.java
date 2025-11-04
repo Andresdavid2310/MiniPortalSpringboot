7c5ebed787271174327eca73eedbd139fb058d4d
INDRA\adbenavides <adbenavides@minsait.com>
2025-11-04 13:03:27 +0100

    first commit

---
+++ src/main/java/com/exercise/MiniPortal/MiniPortalApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.MiniPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniPortalApplication.class, args);
+	}
+
+}
