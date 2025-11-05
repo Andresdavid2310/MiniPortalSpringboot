a9106ec9ede7edcfd10b298304d6dbeb6a8d3bce
INDRA\adbenavides <adbenavides@minsait.com>
2025-11-05 11:28:40 +0100

    add some stile to menu

--- src/main/java/com/exercise/MiniPortal/util/MenuUtil.java
+++ src/main/java/com/exercise/MiniPortal/util/MenuUtil.java
@@ -3,13 +3,14 @@ package com.exercise.MiniPortal.util;
 public class MenuUtil {
     public static String menu() {
         return """
-            <nav>
-                <a href='/portal'>Inicio</a>
-                <a href='/portal/form'>Registro</a>
-                <a href='/portal/dashboard'>Panel</a>
-                <a href='/portal/password-generator'>Generador Contraseñas</a>
-                <a href='/portal/about'>Acerca de</a>
-            </nav><hr>
+            <nav style='display: flex; gap: 30px; background: #f0f0f0; padding: 10px; border-radius: 8px;'>
+                <a href='/portal' style='text-decoration:none; color:#333; font-weight:bold;'>Inicio</a>
+                <a href='/portal/form' style='text-decoration:none; color:#333; font-weight:bold;'>Registro</a>
+                <a href='/portal/dashboard' style='text-decoration:none; color:#333; font-weight:bold;'>Panel</a>
+                <a href='/portal/password-generator' style='text-decoration:none; color:#333; font-weight:bold;'>Generador Contraseñas</a>
+                <a href='/portal/about' style='text-decoration:none; color:#333; font-weight:bold;'>Acerca de</a>
+            </nav>
+            <hr>
         """;
     }
 }
